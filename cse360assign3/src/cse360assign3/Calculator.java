1a00ab6c8257e0cbd033b74d5e7a09cc25aefa22
ashwin <ashwin@ashwin-Lenovo-Y50-70>
2016-02-22 14:09:44 -0700

    Initial Version

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,42 @@
+package cse360assign3;
+/**
+ * Initial Version.
+ * A Class that contains the calculator methods such as 
+ * addition, subtraction, multiplication , division and returns the 
+ * total.
+ * @author Ashwin Murthy PIN - 613 CSE360
+ *
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,52 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Contains Junit Test cases for the class Calculator
+ * @author Ashwin Murthy  PIN - 613  CSE360 Spring 2016
+ *
+ */
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator obj = new Calculator();
+		assertNotNull(obj);
+		// fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testadd() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testsubtract() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testmultiply() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testdivision() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testgetTotal() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testgetHistory() {
+		fail("Not yet implemented");
+	}
+
+}
