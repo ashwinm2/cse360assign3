635371b86848707525f722c1956c929a8cb0ad81
ashwin <ashwin@ashwin-Lenovo-Y50-70>
2016-02-22 14:34:26 -0700

    Second Version

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -1,42 +1,78 @@
 package cse360assign3;
+
 /**
- * Initial Version.
- * A Class that contains the calculator methods such as 
- * addition, subtraction, multiplication , division and returns the 
- * total.
- * @author Ashwin Murthy PIN - 613 CSE360
+ * Second Version.
+ * A Class to perform the basic function of a calculator such as
+ * addition, subtraction, multiplication, division and
+ * return the total.
+ * 
+ * @author Ashwin Murthy PIN - 613  CSE360 Spring 2016
  *
  */
-
 public class Calculator {
-
-	private int total;
 	
+	private int total; 
+	 
+	
+	/** Initializing the total to zero. */ 
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	
+	/** Functions that returns the total.
+	 * @param none
+	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	
+	/** Function that performs the addition operation
+	 * @param Value to be added.
+	 */
 	public void add (int value) {
+		total = total + value;
 		
 	}
 	
+	
+	/** Function that performs the subtraction operation
+	 * @param Value to be subtracted.
+	 */
 	public void subtract (int value) {
+		total = total - value;
 		
 	}
 	
+	
+	/** Function that performs the multiplication operation
+	 * @param Value to be multiplied.
+	 */
 	public void multiply (int value) {
+		total = total * value;
 		
 	}
 	
+	
+	/** Function that performs the division operation.
+	 *  If the value is zero the total is reset.
+	 * @param Value to be divided.
+	 */
 	public void divide (int value) {
+		 if( value != 0 )
+			total = total / value;
+		else
+			total = 0;
 		
 	}
+	 
 	
+	/** Function that returns the history of operation in string format.
+	 * @param none.
+	 */
 	public String getHistory () {
 		return "";
 	}
+
 }
--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -9,39 +9,67 @@ import org.junit.Test;
  * @author Ashwin Murthy  PIN - 613  CSE360 Spring 2016
  *
  */
-
 public class CalculatorTest {
 
 	@Test
-	public void testCalculator() {
+	public void testCalculator () {
+		
 		Calculator obj = new Calculator();
 		assertNotNull(obj);
-		// fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testadd() {
-		fail("Not yet implemented");
+	public void testgetTotal () {
+		Calculator obj = new Calculator();
+		obj.add(15);
+		obj.subtract(6);
+		obj.multiply(2);
+		obj.divide(3);
+		assertEquals("Total must be 14",6,obj.getTotal());
 	}
 	
 	@Test
-	public void testsubtract() {
-		fail("Not yet implemented");
+	public void testadd () {
+		Calculator obj = new Calculator();
+		obj.add(15);
+		obj.add(15);
+		assertEquals("Total must be 30",30,obj.getTotal());
+		//fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testmultiply() {
-		fail("Not yet implemented");
+	public void testsubtract () {
+		Calculator obj = new Calculator();
+		obj.add(15);
+		obj.subtract(5);
+		assertEquals("Total must be 10",10,obj.getTotal());
+		//fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testdivision() {
-		fail("Not yet implemented");
+	public void testmultiply () {
+		Calculator obj = new Calculator();
+		obj.multiply(15);
+		assertEquals("Total must be 0",0,obj.getTotal());
+		//fail("Not yet implemented");
 	}
 	
 	@Test
-	public void testgetTotal() {
-		fail("Not yet implemented");
+	public void testdividecase0 () {
+		Calculator obj = new Calculator();
+		obj.add(15);
+		obj.divide(0);
+		assertEquals("Total must be 0",0,obj.getTotal());
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testdivide () {
+		Calculator obj = new Calculator();
+		obj.add(15);
+		obj.divide(5);
+		assertEquals("Total must be 3",3,obj.getTotal());
+		//fail("Not yet implemented");
 	}
 	
 	@Test
